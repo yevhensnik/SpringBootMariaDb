acfe7c56a9b3477f3bc18ade790aa711fe3cdb49
yevhensnik <yevhengren@gmail.com>
2024-02-10 00:39:03 +0200

    init commit

---
+++ src/main/java/com/springbootmariadb/SpringBootMariaDbApplication.java
@@ -0,0 +1,13 @@
+package com.springbootmariadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMariaDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMariaDbApplication.class, args);
+	}
+
+}
